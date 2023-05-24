acd007bbc85146ab9fed8dc8387559c52d327a10
Dora Akbulut <akbulutdora@gmail.com>
2023-05-24 23:49:28 +0100

    Initial commit

---
+++ blog/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Build the book
+mdbook build
+# Switch to the main branch
+git checkout main
+# Copy the contents of the book directory to the root directory
+cp -r ./book/* ../.
+# Add all new files
+git add .
+# Commit the changes
+git commit -m "Update blog"
+# Push the changes to GitHub
+git push origin main
